6c97b6e377ee2355b2b1c121b96472874456df06
Syed Razvi <103219618@swin.edu.au>
2020-06-08 00:45:37 +1000

    classes

--- src/bookmarkclasses.ts
+++ src/bookmarkclasses.ts
@@ -1,29 +1,82 @@
-export class Bookmark
-{
+export class Bookmark {
 
-    Title: string;
-    URL: string;
+    nameField: string;
+    urlField: string;
     icon: string;
     favourite: boolean = false;
 
-    constructor (webtitle: string, ficon: string){
-        this.Title = webtitle;
-        this.URL = "";
+    constructor(title: string, url: string) {
+        this.nameField = title;
+        this.urlField = url;
         this.icon = "";
-        let favIcon = ficon;
-
-    
     }
 
-    setURL(id: string){
-        id = this.URL
-        
+    CreateMyBookmark(nameField1: string, urlField1: string) {
+
+
+        let webName = nameField1;
+        let webUrl = urlField1;
+
+
+        let myNewElement = document.createElement("div");
+        myNewElement.classList.add("itemList");
+
+        // Set the icon for the website
+        //let favicon = nameField2.value;
+        let icon = `https://s2.googleusercontent.com/s2/favicons?domain=${webUrl}`;
+
+        let img = document.createElement("img") as HTMLImageElement;
+        img.setAttribute('src', icon);
+        myNewElement.appendChild(img);
+
+
+
+        let myNewElementText1 = document.createElement("h5");
+        myNewElementText1.appendChild(document.createTextNode(webName));
+        myNewElement.appendChild(myNewElementText1);
+        nameField.value = ""
+
+        // setting url link
+        let url = document.createElement("a");
+        let url2 = webUrl;
+        url.setAttribute('href', url2)
+        url.classList.add("urlDisplay");
+        url.appendChild(document.createTextNode(url2));
+        myNewElement.appendChild(url);
+        urlField.value = ""
+
+        // icon setup
+        let favourIcon = document.createElement("a");
+        favourIcon.href = "#";
+        favourIcon.innerHTML = '<i class="far fa-heart"></i>';
+        favourIcon.classList.add("favourIcon")
+        myNewElement.appendChild(favourIcon);
+
+
+        //favouriteIcon.innerHTML = '<i class="fas fa-heart"></i>';
+        favourIcon.onclick = function () {
+            let state1 = '<i class="far fa-heart"></i>';
+            let state2 = '<i class="fas fa-heart"></i>';
+            if (favourIcon.innerHTML == state1) {
+                favourIcon.innerHTML = '<i class="fas fa-heart"></i>';
+            } else if (favourIcon.innerHTML == state2) {
+                favourIcon.innerHTML = '<i class="far fa-heart"></i>';
+            }
         }
-        
-    getURL(){
-        if (this.URL == null) {
-            return "";
+
+        // set the delete icon
+        let a = document.createElement("a");
+        a.href = "#";
+        a.innerHTML = '<i class="fas fa-trash-alt"></i>';
+        myNewElement.appendChild(a);
+
+        elements.appendChild(myNewElement);
+
+        // Bookmark Delete Function
+        a.onclick = function () {
+            let del = a.parentElement as HTMLElement;
+            if (del == null) return;
+            del.remove();
         }
-        return this.URL
-        }
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-// import {Bookmark} from 
+import { Bookmark } from "./bookmarkclasses";
 
 let addButton = document.getElementById("addButton") as HTMLButtonElement | null;
 let elements = document.getElementById("elements") as HTMLElement;
-let nameField = document.getElementById("nameField") as HTMLDataElement;
-let urlField = document.getElementById("urlField") as HTMLDataElement;
+let nameField = document.getElementById("nameField") as HTMLInputElement;
+let urlField = document.getElementById("urlField") as HTMLInputElement;
+
+
+let google = new Bookmark("Google", "www.google.com");
+google.CreateMyBookmark("Google", "www.google.com");
 
 if (addButton == null || undefined) {
     alert("Button not found.");
@@ -11,65 +15,8 @@ if (addButton == null || undefined) {
 else {
 
     addButton.onclick = function () {
-        let myNewElement = document.createElement("div");
-        myNewElement.classList.add("itemList");
 
-        // Set the icon for the website
-        let favicon = urlField.value;
-        let icon = `https://s2.googleusercontent.com/s2/favicons?domain=${favicon}`;
-
-        let img = document.createElement("img") as HTMLImageElement;
-        img.setAttribute('src', icon);
-        myNewElement.appendChild(img);
-
-
-
-        let myNewElementText1 = document.createElement("h5");
-        myNewElementText1.appendChild(document.createTextNode(nameField.value));
-        myNewElement.appendChild(myNewElementText1);
-        nameField.value = ""
-
-        // setting url link
-        let url = document.createElement("a");
-        let url2 = urlField.value;
-        url.setAttribute('href', url2)
-        url.classList.add("urlDisplay");
-        url.appendChild(document.createTextNode(url2));
-        myNewElement.appendChild(url);
-        urlField.value = ""
-
-        // icon setup
-        let favourIcon = document.createElement("a");
-        favourIcon.href = "#";
-        favourIcon.innerHTML = '<i class="far fa-heart"></i>';
-        favourIcon.classList.add("favourIcon")
-        myNewElement.appendChild(favourIcon);
-
-
-        //favouriteIcon.innerHTML = '<i class="fas fa-heart"></i>';
-        favourIcon.onclick = function () {
-            let state1 = '<i class="far fa-heart"></i>';
-            let state2 = '<i class="fas fa-heart"></i>';
-            if (favourIcon.innerHTML == state1) {
-                favourIcon.innerHTML = '<i class="fas fa-heart"></i>';
-            } else if (favourIcon.innerHTML == state2) {
-                favourIcon.innerHTML = '<i class="far fa-heart"></i>';
-            }
-        }
-
-        // set the delete icon
-        let a = document.createElement("a");
-        a.href = "#";
-        a.innerHTML = '<i class="fas fa-trash-alt"></i>';
-        myNewElement.appendChild(a);
-
-        elements.appendChild(myNewElement);
-
-        // Bookmark Delete Function
-        a.onclick = function () {
-            let del = a.parentElement as HTMLElement;
-            if (del == null) return;
-            del.remove();
-        }
+        let mybookmark = new Bookmark(nameField.value, urlField.value);
+        mybookmark.CreateMyBookmark(nameField.value, urlField.value);
     }
 }
