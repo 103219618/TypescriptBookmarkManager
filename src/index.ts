63ea1bc9314ed159b54b4cd3dbf7769a9aa5a2ef
Syed Razvi <103219618@swin.edu.au>
2020-06-07 17:07:47 +1000

    items

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,38 @@
-let myButton = document.getElementById("myButton") as HTMLButtonElement | null;
-if (myButton == null) {
+let addButton = document.getElementById("addButton") as HTMLButtonElement | null;
+let elements = document.getElementById("elements") as HTMLElement;
+let nameField = document.getElementById("nameField") as HTMLDataElement;
+let urlField = document.getElementById("urlField") as HTMLDataElement;
+
+if (addButton == null || undefined) {
     alert("Button not found.");
-} else {
-    
-    myButton.onclick = function () {
-        if(myButton == undefined) return;
-        alert("Hello World!");
+}
+else {
+
+    addButton.onclick = function () {
+        let myNewElement = document.createElement("div");
+        myNewElement.classList.add("itemList");
+
+        let myNewElementText1 = document.createElement("h5");
+        myNewElementText1.appendChild(document.createTextNode(nameField.value));
+        myNewElement.appendChild(myNewElementText1);
+        nameField.value = ""
+
+        let myNewElementText2 = document.createElement("h5");
+        myNewElementText2.appendChild(document.createTextNode(urlField.value));
+        myNewElement.appendChild(myNewElementText2);
+        urlField.value = ""
+
+        elements.appendChild(myNewElement);
+        
+        
+        
+        if (clrButton == null) {
+            alert("Button not found.");
+        }
+        else {
+            clrButton.onclick = function () {
+                elements.innerHTML = "";
+            }
+        }
     }
 }
